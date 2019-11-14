7ed6924e970bb1156ae5ae972e6c744349aaa262
gsimms <gsimms@kuttatech.com>
2019-11-14 08:26:20 -0700

    syncing with my laptop

---
+++ BalloonFight-Gym/balloonfight.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import retro
+'''
+The idea behind this file is that I want to combine:
+- balloon fight
+- reinforcement (policy gradient)
+- dense layers (hold off on convolutional neural net)
+- Keras/Tensorflow (2.0?)
+
+
+'''
+
+import tensorflow as tf
+from tensorflow import keras
+
