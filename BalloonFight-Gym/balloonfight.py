49a6a514e970ad686f4c3d75dcbca4e364d9b2c8
Athanasius Simms <athanasius@sim.ms>
2020-01-07 19:43:42 -0700

    night night

--- BalloonFight-Gym/balloonfight.py
+++ BalloonFight-Gym/balloonfight.py
@@ -11,6 +11,34 @@ The idea behind this file is that I want to combine:
 
 '''
 
-import tensorflow as tf
-from tensorflow import keras
+from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy, MlpLnLstmPolicy, CnnLnLstmPolicy, CnnPolicy, CnnLstmPolicy
+from stable_baselines.common.vec_env import SubprocVecEnv, DummyVecEnv
+from stable_baselines import PPO2, A2C
 
+#env = DummyVecEnv([lambda: retro.make('BalloonFight-Nes', record='/gdrive/My Drive/science fair project/2019/BalloonFightRecord/')])
+env = DummyVecEnv([lambda: retro.make('BalloonFight-Nes')])
+
+modelname = 'balloonfightppo'
+model = PPO2(CnnPolicy,env,n_steps=2048, verbose=1)
+#model = PPO2.load("BalloonFightRecord/" + modelname+".pkl")
+for saveloop in range(100):
+  print("saveloop = "+str(saveloop))
+  for learnloop in range(15):
+    print("learnloop = "+str(learnloop))
+    model.learn(total_timesteps=10000)
+  print("saving "+str(modelname))
+  model.save("BalloonFightRecord/" + modelname)
+
+'''
+obs = env.reset()
+done = False
+reward = 0
+
+while not done:
+  actions, _ = model.predict(obs)
+  obs, rew, done, info = env.step(actions)
+  reward += rew
+
+  
+print(rew)
+'''
